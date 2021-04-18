d5da6738f2e5249e585321a337bf951286e98e57
SwastiBansal <82759999+SwastiBansal@users.noreply.github.com>
2021-04-18 04:57:09 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic Banking System</title>
+    <style type="text/css">
+<!--
+.style1 {
+	color: #000000;
+	font-family: raleway;
+}
+.style2 {
+	font-family: raleway;
+	font-weight: bold;
+	font-size: 36px;
+	font-style: italic;
+}
+.style3 {
+	font-family: raleway;
+	font-weight: bold;
+	font-style: italic;
+	font-size: 18px;
+}
+.style4 {color: #000000; font-family: raleway; font-size: 24px; }
+-->
+    </style>
+</head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div class="row intro py-1">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h3 class="style4">&nbsp;</h3>
+                  <h3 class="style2"> TSF BANK </h3>
+                  <h3 class="style1">Money Transfer got easy. </h3>
+                  <h1 class="style4">Transfer your money with ease. </h1>
+                  <p align="center">&nbsp;</p>
+                </div>
+              </div>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <p>&nbsp;</p>
+              <div class="col-sm-12 col-md img text-center"><img src="img/money_trans.png" width="550" height="482" class="img-fluid pt-2"></div>
+            </div>
+
+      <!-- Activity section -->
+            <div class="row activity text-center">
+                  <div class="col-md act"><img src="img/user_1.jfif" width="139" height="134" class="img-fluid"><br>
+                    <a href="createuser.php"><button>Create a User</button></a>
+              </div>
+                  <div class="col-md act"><img src="img/5.png" width="255" height="169" class="img-fluid"><br>
+                    <a href="transfermoney.php"><button>Make a Transaction</button></a>
+              </div>
+                  <div class="col-md act"><img src="img/histo.jpg" width="129" height="120" class="img-fluid"><br>
+                    <a href="transactionhistory.php"><button>Transaction History</button></a>
+              </div>
+            </div>
+      </div>
+      <footer class="text-center mt-5 py-2">
+        <p>&copy 2021. Made by <b>SWASTI BANSAL</b> <br> The Sparks Foundation</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!-- navbar -->
+<style type="text/css">
+<!--
+.style3 {
+	font-size: 36px;
+	font-family: raleway;
+	font-style: italic;
+	font-weight: bold;
+}
+.style4 {font-family: raleway}
+-->
+</style>
+ 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <p><span class="style3"><a href="index.php" class="navbar-brand">TSF BANK </a></span></p>
+        <p><a href="index.php" class="nav-link style4">Home</a></p>
+        <p>
+          <a href="createuser.php" class="nav-link style4">Create User </a></p>
+        <p>
+          <a href="transfermoney.php" class="nav-link style4">Transfer Money </a></p>
+        <p>
+          <a href="transactionhistory.php" class="nav-link style4">Transaction History </a></p>
+        </div>
+</nav>
